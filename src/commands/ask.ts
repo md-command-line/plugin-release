b8898932d8ed9cf8e4c1164fda68848b46ec6f0b
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-23 12:39:24 -0400

    command checks to see if application is valid.

---
+++ src/commands/ask.ts
@@ -0,0 +1,20 @@
+import {Command, flags} from '@heroku-cli/command'
+import * as Heroku from '@heroku-cli/schema'
+
+// for future use: /apps/{app_id_or_name}/releases/{release_id_or_version}
+export default class Ask extends Command {
+
+  static description = 'show application name if found'
+  static exampleCommand = 'heroku ask -a intense-crag-70741'
+
+  static flags = {
+    remote: flags.remote(),
+    app: flags.app({required: true})
+  }
+
+  async run() {
+    const {flags} = this.parse(Ask)
+    let {body}:any = await this.heroku.get<Heroku.App>(`/apps/${flags.app}/`).catch(x =>{this.log(x.body); throw "exit"})
+    body ? console.log(body.name) : console.log('application name incorrect')
+  }
+}
