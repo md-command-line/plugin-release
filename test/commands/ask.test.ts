e8e9e650767990c10e97348853e23bace1c926f7
Michael Dimmitt <michaeldimmitt@Michaels-MacBook-Pro.local>
2020-08-04 18:23:05 -0400

    temp fix tests

--- test/commands/ask.test.ts
+++ test/commands/ask.test.ts
@@ -1,10 +1,18 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
-describe('check application connectivity', () => {
+describe('check -a application connectivity', () => {
   test
-  .stdout()
-  .command(['ask', '-a', 'intense-crag-70741'])
-  .it('show application name', ctx => {
-    expect(ctx.stdout).to.contain('intense-crag-70741')
-  })
+    .stdout()
+    .command(['ask', '-a', 'repository-dashboard'])
+    .it('show application name', ctx => {
+      expect(ctx.stdout).to.contain('repository-dashboard')
+    })
+})
+describe('check --all application connectivity', () => {
+  test
+    .stdout()
+    .command(['ask', '--all', 'repository-dashboard'])
+    .it('show application name', ctx => {
+      expect(ctx.stdout).to.contain('repository-dashboard')
+    })
 })
